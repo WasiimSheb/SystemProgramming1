e8edb0737a973f0b50680501a39e47a5bde706e9
WasiimSheb <123734906+WasiimSheb@users.noreply.github.com>
2024-01-28 13:13:51 +0200

    Update advancedClassificationRecursion.c

--- advancedClassificationRecursion.c
+++ advancedClassificationRecursion.c
@@ -1,6 +1,12 @@
 
-int power(int n, int y);
-
+int power2(int n, int y) {
+    int  res = 1;
+    while (y > 0) {
+        res *= n;
+        y--;
+    }
+    return res;
+}
 int countDigits(int num) {
     if (num == 0) {
         return 0;
@@ -14,7 +20,7 @@ int isArmstrongRecursive(int num, int originalNum, int numDigits) {
     }
 
     int digit = num % 10;
-    return (power(digit, numDigits) + isArmstrongRecursive(num / 10, originalNum, numDigits));
+    return (power2(digit, numDigits) + isArmstrongRecursive(num / 10, originalNum, numDigits));
 }
 
 int isArmstrong2(int num) {
@@ -26,14 +32,14 @@ int isPalindromeRecursive(int num, int numDigits) {
         return 1;
     }
 
-    int firstDigit = num / power(10, numDigits - 1);
+    int firstDigit = num / power2(10, numDigits - 1);
     int lastDigit = num % 10;
 
     if (firstDigit != lastDigit) {
         return 0;
     }
 
-    num = (num % power(10, numDigits - 1)) / 10;
+    num = (num % power2(10, numDigits - 1)) / 10;
 
     return isPalindromeRecursive(num, numDigits - 2);
 }
