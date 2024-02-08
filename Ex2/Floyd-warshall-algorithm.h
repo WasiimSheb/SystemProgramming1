6c10ea31a82a59a5d72fa0835e8bead912d74348
WasiimSheb <123734906+WasiimSheb@users.noreply.github.com>
2024-02-08 11:58:00 +0200

    Update and rename Floyd-warshall-algorithm.h to my_mat.h

--- Ex2/Floyd-warshall-algorithm.h
+++
@@ -1,5 +0,0 @@
-
-#define V 10
-int thereisapath(int graph[V][V], int src, int dist);
-void shortestpath(int graph[V][V]);
-void initvalues(int graph[V][V]);
---
+++ Ex2/my_mat.h
@@ -0,0 +1,7 @@
+
+#include <stdbool.h>
+
+#define V 10
+bool thereisapath(int graph[V][V], int src, int dest);
+int shortestpath(int graph[V][V], int src, int dest);
+void initvalues(int graph[V][V]);
